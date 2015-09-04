628a1ca7861cfc015ad097986b8dc5fce8e6c33b
Andrew Hamon <andrew@hamon.cc>
2015-09-04 17:38:14 -0400

    Remove google analytics, tweak font settings, move script to end of body

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,5 @@
-var $         = require('jquery');
 var main_page = require('./ily.js');
 var new_page  = require('./new.js');
 
-$( document ).ready(function (){
-  main_page()
-  new_page()
-});
+main_page()
+new_page()
