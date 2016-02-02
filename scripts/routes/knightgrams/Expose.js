55ca4f3afd044b1ab17d17967cfc41e03dca7cac
Steven Petryk <me@stevenpetryk.com>
2016-02-02 17:11:32 -0500

    Change the copy on the Expose and add some margin

--- scripts/routes/knightgrams/Expose.js
+++ scripts/routes/knightgrams/Expose.js
@@ -9,21 +9,21 @@ export default React.createClass({
             <img src='images/knightgrams/gram@2x.png' width='153' />
 
             <h3>1. Pick their poison.</h3>
-            <p>Chocolate, candy, or a rose.</p>
+            <p>Chocolate, candy, or a rose. This will accompany a note of your choosing.</p>
           </article>
 
           <article>
             <img src='images/knightgrams/phone@2x.png' width='66' />
 
             <h3>2. Give us their number.</h3>
-            <p>We'll text your crush with instructions.</p>
+            <p>We'll text your Valentine with the good news and information about the pickup. You'll remain anonymous.</p>
           </article>
 
           <article>
             <img src='images/knightgrams/banner@2x.png' width='34' />
 
             <h3>3. They get some love.</h3>
-            <p>Pickup takes place on campus.</p>
+            <p>Pickup takes place at the Student Union, starting on Wednesday, February 10th (specific times TBA).</p>
           </article>
         </div>
       </div>
